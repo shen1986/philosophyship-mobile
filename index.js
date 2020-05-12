380cce773a1a4d1e4c1e15ed0306b4d102aaa74c
shenxf1986 <185693240@qq.com>
2020-05-12 10:30:11 +0800

    update

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: Shenxf 
+ * @Date: 2019-08-15 22:19:05 
+ * @Last Modified by: Shenxf 
+ * @Last Modified time: 2019-08-15 22:19:05 
+ */
+
+import 'font-awesome/css/font-awesome.min.css';
+import './css/tuitui-ui.min.css';
